ab63d95dbc65d6924bfbb8777fef79a3bf01bfe7
mayank chaurasia <chaurasiamayank637@gmail.com>
2024-09-26 09:24:53 +0000

    conversion of data types

---
+++ .devcontainer/basics_01/type_conversion_01.js
@@ -0,0 +1,32 @@
+let marks ="false";
+console.log(typeof marks);
+
+let marksInNumber = Number(marks)
+console.log(marksInNumber);
+
+console.log(marksInNumber);
+/*
+"33" => 33
+"33mayank" => NaN
+true => 1; false => 0;
+null => 0; undefined => NaN;
+*/
+
+let isloggedIn = 1
+
+let booleanisloggedIn = Boolean(isloggedIn)
+console.log(typeof booleanisloggedIn);
+console.log(booleanisloggedIn);
+
+/*
+(empty string)"" => 0;  "mayank" => 1;
+1 => true;  0 =>false;
+accept 0 all number => true;
+*/
+
+let a = 25
+let a_string = String(a)
+console.log(typeof a_string);
+console.log(a_string);
+// number => easily convert to string without confusing
+
