65a7cc0cec0d8d6b64d0bd1614f98ee3e12bc171
mayank chaurasia <chaurasiamayank637@gmail.com>
2024-09-26 08:21:49 +0000

    add somethng in variable_01 file

---
+++ .devcontainer/basics_01/variable_01.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+// #1_variable
+console.log("hello world")
+const accountId = 144553;
+let accountEmail = "mayank@gogle.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+accountEmail = "mkc@google.com"
+accountPassword = "54123"
+accountCity = "chhatarpur"
+let accountState;
+let a = 2.2
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState, a]);
+
+// console.log(accountId);
+
+// #2_dataType
+console.log(typeof undefined);
