9290ad698df9a6a171eb7f9b9b0601ab63780a31
mayank chaurasia <chaurasiamayank637@gmail.com>
2024-09-26 08:27:11 +0000

    explaination of data types in variable_01 file

--- .devcontainer/basics_01/variable_01.js
+++ .devcontainer/basics_01/variable_01.js
@@ -19,3 +19,10 @@ console.table([accountId, accountEmail, accountPassword, accountCity, accountSta
 
 // #2_dataType
 console.log(typeof undefined);
+
+let name = "mayank" //string
+let age = 18 // number
+let isloggedin = true //boolean
+let state; // undefined
+let marks = null // object
+
