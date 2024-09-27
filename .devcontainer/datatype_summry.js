a3d4a2fb3047bf0055b0ff39703bf7527febe80e
itx-mayank-chaurasia <chaurasiamayank637@gmail.in>
2024-09-27 12:05:15 +0530

    add datatype summry

---
+++ .devcontainer/datatype_summry.js
@@ -0,0 +1,29 @@
+// MOSTLY DATA TYPE ARE TWO TYPES
+
+// #1 primitive data type 7types ==> number, string, boolean, null, undefined, symbol, bigInt 
+// #2 reffrence (not-primitive data type) 3types ==> array, object, function {data type is => object}
+
+// let number = 10 #number
+// let name = "mayank" # string
+// let isnumber = true #boolean
+// let temprature = null #object
+// let roll_number; #undefined
+// const bignumber = 101214851364313154968n #bigInt
+// const score1 = Symbol('221') they are 7 primite data types #Symbol
+// const score2 = Symbol('221') #Symbol
+
+// console.log(score1===score2); false beacuse they are tottly diff 
+// console.log(typeof bignumber); this is a investigation
+
+// let myarray = ["mayank", "sanskar", "krishna", "radha"] array => datatype is object
+// console.log(myarray);
+
+// let myobj = {
+//     str: "str1", object => datatype is object
+//     age:18
+// }
+// console.log(myobj);
+
+// const myfunction = function(){
+//     console.log("hello world"); function => datatype is object
+// }
