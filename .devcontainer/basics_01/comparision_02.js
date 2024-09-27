43c20911995bdc307ce9fe5a46399f4686e2d63c
itx-mayank-chaurasia <chaurasiamayank637@gmail.in>
2024-09-27 10:15:49 +0530

    add comprasion file

---
+++ .devcontainer/basics_01/comparision_02.js
@@ -0,0 +1,21 @@
+// console.log(2==3)
+// console.log(2<3)
+// console.log(2>3)
+// console.log(2<=3) *******it is a normal comparision ***********
+// console.log(2>=3)
+// console.log(2!=3)
+
+
+// console.log("2">=3)
+// console.log(4>="3") ******convert into number*********
+
+// console.log(null > 0);****
+// console.log(null < 0);******* they all thre can big 0f 0 and equal of 0******
+// console.log(null >= 0);***
+// console.log(null == 0); *****convert into 0******
+
+// console.log(undefined >0)*****
+// console.log(undefined <0)******** all false because*******
+// console.log(undefined ==0)****
+
+// console.log("2" === 2); *****triplle equal to check the value with data type****
