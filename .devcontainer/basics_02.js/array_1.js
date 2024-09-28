f5adc33ae89fcdfea9cd7aa4818598b7c6196889
itx-mayank-chaurasia <chaurasiamayank637@gmail.in>
2024-09-28 22:17:16 +0530

    add theory of array part 1

---
+++ .devcontainer/basics_02.js/array_1.js
@@ -0,0 +1,28 @@
+/* const array1 = [1, 2,  3, 4, 5, 6, 7, 8, 9, 10]; // first method to declare array
+console.log(array1);
+const array2 = ["mayank", "sanskar", "som", "adrsh", "rajju", "divyansh"]; //aecond method to declare array
+console.log(array2[2]); */
+const array3 = new Array(0, 1, 2,  3, 4, 5, 6, 7, 8, 9, 10); // third method to declare array with an obj.
+// console.log(array3);
+// console.log(array2.indexOf(1));
+// array3.push(11); // add new item in array (starting se)
+// console.log(array3);
+// array3.pop();    //  delete last index item in array (staring side se)
+// console.log(array3);
+
+// array3.unshift(0);  //  add new item in array (ending se)
+// console.log(array3);
+// array3.shift();        // delete last index item in array (ending side se)
+// console.log(array3);
+
+// console.log(array3.includes(9)); // for check somethig is includes in array or not
+// console.log(array3.indexOf(1)); // for check which element exist in which index 
+// console.log(array3);
+// console.log(typeof array3);
+// console.log(array3.join()); // convert array into string
+// console.log(typeof array3.join());
+// console.log(array3);
+// console.log("A ", array3.slice(1,5)); // kis index se kis index tk value print krni hai (last index not print)
+// console.log(array3);
+// console.log("b ", array3.splice(1,5)); //  // kis index se kis index tk value print krni hai (last index also will be print) splice main array ko manuplate kar deta hai means jo value splice ke sath print kr lete hai vo main array se delete ho jati hai
+
