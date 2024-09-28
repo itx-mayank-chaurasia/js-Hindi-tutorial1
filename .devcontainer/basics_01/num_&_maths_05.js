bea3679d47fc4d42606adb9232dfb664f72da821
itx-mayank-chaurasia <chaurasiamayank637@gmail.in>
2024-09-28 15:29:04 +0530

    remove comments in num and maths file

--- .devcontainer/basics_01/num_&_maths_05.js
+++ .devcontainer/basics_01/num_&_maths_05.js
@@ -1,4 +1,4 @@
-/* const num = 100
+const num = 100
 const new_num = new Number(100.213) //number as a object
 console.log(num,new_num);
 console.log(typeof new_num.toString()); //convert to string and type also change in string output "string [100]"
@@ -16,9 +16,9 @@ console.log(Math.ceil(25.01)); // decimal se thoda sa bhi jyada hua to use bade 
 console.log(Math.floor(25.9)); // decimal se kitna bhi  jyada h0 to use chhote digit me round kr dega
 console.log(Math.sqrt(144)); //find sqrt
 console.log(Math.min(2,5,8,5,2,4,)); //find minimum
-console.log(Math.max(2,5,8,5,2,4,)); // find maximum */
+console.log(Math.max(2,5,8,5,2,4,)); // find maximum 
 console.log(Math.floor(Math.random() * 10) +1);
 
 const min =1
 const max =9999
-console.log(Math.floor(Math.random() * (max-min)) + min);
+console.log(Math.floor(Math.random() * (max-min)) + min);  //creat any random number (uses i.g. otp)
