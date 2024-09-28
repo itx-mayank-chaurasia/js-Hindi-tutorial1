31f7e55ee46b80de407d17ff579ef562ddda96f3
itx-mayank-chaurasia <chaurasiamayank637@gmail.in>
2024-09-28 20:37:15 +0530

    add date and time theory in dateinjs.js file

--- .devcontainer/basics_01/datesinjs_06.js
+++ .devcontainer/basics_01/datesinjs_06.js
@@ -1,5 +1,5 @@
 let myDate = new Date() //  current date
-/* console.log(myDate); // 2024-09-28T10:10:18.316Z
+console.log(myDate); // 2024-09-28T10:10:18.316Z
 console.log(myDate.toISOString()); // 2024-09-28T10:11:35.947Z
 console.log(myDate.toString()); // Sat Sep 28 2024 15:43:28 GMT+0530 (India Standard Time)
 console.log(myDate.toUTCString()); // Sat, 28 Sep 2024 10:16:56 GMT
@@ -17,8 +17,8 @@ console.log(MyDate.toDateString()); // for creat date ourself
 let Timestamp = Date.now();        //****  1 jan 1970 se abhi tk ki date in mili sec.
 console.log(Math.floor(Timestamp));//****  and print
 
-let MycreatedDate = new Date("06-11-2005"); // for creat date ourself
-console.log(Math.floor(MycreatedDate.getTime()));    */  // 1 jan se apne dvara di gai date tak ka time in mili sec.
+let Mycreated = new Date("06-11-2005"); // for creat date ourself
+console.log(Math.floor(MycreatedDate.getTime()));      // 1 jan se apne dvara di gai date tak ka time in mili sec.
 
 let MycreatedDate = new Date("11-06-2005"); //"mm-dd-yy" iske andar likhne par vah array jese behave karta hai isliye get + 1 krte hai
 console.log(MycreatedDate.getMonth() + 1);   // to get time, date, month, year, full year, i.g.
