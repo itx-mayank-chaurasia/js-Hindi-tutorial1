e5a381716a7aff7f420769d492f0eeb03b42a909
itx-mayank-chaurasia <chaurasiamayank637@gmail.in>
2024-10-02 22:04:43 +0530

    while and do-while loop

---
+++ .devcontainer/iteration_05/while-do-whileloop.js
@@ -0,0 +1,21 @@
+// while loop
+let i = 2;
+// console.log("all even number till 10 is:=>");
+
+while (i <= 11) {
+    // console.log(i)
+    i = i+2;
+}
+let j = 0;
+let myarray = ["kanha", "radha", "madhav"];
+while(j < myarray.length) {
+    // console.log(`my fav. god name is:=> ${myarray[j]} `);
+    j++;
+}
+
+let marks = 50;
+do{
+    console.log(`your performance is good and your marks is${marks}`);
+    marks = marks + 10;
+    
+}while (marks <= 90);
