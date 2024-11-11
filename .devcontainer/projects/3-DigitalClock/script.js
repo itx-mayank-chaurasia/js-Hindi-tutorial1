7a2d121ac0b9f46ed62a22cca6710fb5c849cb72
mayank chaurasia <chaurasiamayank637@gmail.com>
2024-11-11 20:10:25 +0530

    Delete .devcontainer/projects directory

--- .devcontainer/projects/3-DigitalClock/script.js
+++
@@ -1,6 +0,0 @@
-const clock = document.querySelector('.clock');
-let date = new Date();
-setInterval( function() {
-    let date = new Date();
-    clock.innerHTML = date.toLocaleTimeString();
-},1000 )
