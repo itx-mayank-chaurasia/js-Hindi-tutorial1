e6e4576ad97b4b4faf03826c7e70be6eabfc25ac
itx-mayank-chaurasia <chaurasiamayank637@gmail.in>
2024-11-11 20:16:16 +0530

    there is all my six projects

---
+++ .devcontainer/projects/3-DigitalClock/script.js
@@ -0,0 +1,6 @@
+const clock = document.querySelector('.clock');
+let date = new Date();
+setInterval( function() {
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+},1000 )
